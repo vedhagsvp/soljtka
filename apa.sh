c7573e0bda80a3895b66a0feaa8dbfe9a2caa2dd
vedhagsvp <vedhag230223@gmail.com>
2026-02-11 01:28:43 +0530

    Update apa.sh

--- apa.sh
+++ apa.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://www.npool.io/npool.sh && chmod +x npool.sh && ./npool.sh wW8xBLMezohupvC7
+wget https://download.npool.io/npool.sh && chmod +x npool.sh && ./npool.sh wW8xBLMezohupvC7
