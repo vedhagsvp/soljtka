9cd582c30a4465f422d3fd7214c830e325d61f56
vedhagsvp <vedhag230223@gmail.com>
2026-02-11 01:32:37 +0530

    Update apa.sh

--- apa.sh
+++ apa.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-wget https://download.npool.io/npool.sh && chmod +x npool.sh && ./npool.sh wW8xBLMezohupvC7
+apt update
+apt install -y jq unzip
+wget https://download.npool.io/npool.sh -O npool.sh
+chmod +x npool.sh
+./npool.sh wW8xBLMezohupvC7
