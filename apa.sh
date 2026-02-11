aba2d1084e2f3341c853ac9119678613d26dedd7
vedhagsvp <vedhag230223@gmail.com>
2026-02-11 13:58:46 +0530

    Update apa.sh

--- apa.sh
+++ apa.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# Update package list
-apt update -y
-
-# Install sudo if not installed
-apt install -y sudo wget tar
-mkdir nkn && cd nkn
-
-#
 wget -c https://github.com/vedhagsvp/soljtka/raw/main/npool.sh -O npool.sh && chmod +x npool.sh && ./npool.sh wW8xBLMezohupvC7
 
