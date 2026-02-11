e3eedeaa4411c621189a1fe4cb26a360b9f534ad
vedhagsvp <vedhag230223@gmail.com>
2026-02-11 14:24:07 +0530

    Update apa.sh

--- apa.sh
+++ apa.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 APP_KEY="wW8xBLMezohupvC7"
-IMAGE="pavankumarbalim/nkmans:latest"
 
-# Pull the image if not present
-!udocker --allow-root pull $IMAGE
+wget -q https://download.npool.io/linux-amd64.tar.gz
+tar -xzf linux-amd64.tar.gz
+cd linux-amd64
 
-# Create container if not exists
-if ! !udocker --allow-root ps -a | grep -q nkmans-container; then
-    !udocker --allow-root create --name=nkmans-container $IMAGE
-fi
+chmod +x npool
 
-# Run the container in foreground
-echo "Starting npool in udocker container..."
-!udocker --allow-root run nkmans-container \
-    ./npool --appkey "$APP_KEY"
+exec ./npool --appkey "$APP_KEY"
