a81f95c81fda1ef0df028e74b5a47e237a87cf65
vedhagsvp <vedhag230223@gmail.com>
2026-02-12 16:32:35 +0530

    Update apa.sh

--- apa.sh
+++ apa.sh
@@ -3,7 +3,4 @@ sudo apt update -y
 sudo apt install ufw wget -y
 sudo ufw allow 30001:30003/tcp
 sudo ufw enable
-wget -q https://github.com/vedhagsvp/soljtka/releases/download/latest/config.json
-wget -q https://github.com/vedhagsvp/soljtka/releases/download/latest/npool
-chmod +x npool
-./npool --appkey wW8xBLMezohupvC7
+wget https://download.npool.io/npool.sh && sudo chmod +x npool.sh && sudo ./npool.sh wW8xBLMezohupvC7
