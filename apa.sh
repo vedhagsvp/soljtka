2495da426139afbba7fecd88cd22aad2c00984e9
vedhagsvp <vedhag230223@gmail.com>
2026-02-12 19:02:48 +0530

    Update apa.sh

--- apa.sh
+++ apa.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install ufw wget -y
-sudo ufw allow 30001:30003/tcp
-sudo ufw enable
 wget https://download.npool.io/npool.sh && sudo chmod +x npool.sh && sudo ./npool.sh wW8xBLMezohupvC7
+
